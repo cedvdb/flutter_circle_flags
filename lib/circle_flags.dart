511fb46a2f934099a2312b25f6116dea7d8b8d6e
Xavier Hainaux <xavier.hainaux@gmail.com>
2023-06-27 15:58:51 +0200

    Use optimized vector format

--- lib/circle_flags.dart
+++ lib/circle_flags.dart
@@ -1,7 +1,7 @@
 library circle_flags;
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vector_graphics/vector_graphics.dart';
 
 /// a rounded flag
 class CircleFlag extends StatelessWidget {
@@ -10,17 +10,16 @@ class CircleFlag extends StatelessWidget {
   final String assetName;
 
   CircleFlag(this.countryCode, {Key? key, this.size = 48})
-      : assetName = 'assets/svg/${countryCode.toLowerCase()}.svg',
+      : assetName = 'assets/optimized/${countryCode.toLowerCase()}.svg.vec',
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ClipOval(
-      child: SvgPicture.asset(
-        assetName,
+      child: VectorGraphic(
+        loader: AssetBytesLoader(assetName, packageName: 'circle_flags'),
         width: size,
         height: size,
-        package: 'circle_flags',
       ),
     );
   }
