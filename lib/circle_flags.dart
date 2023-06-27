4f00180ec3d5d4be9e223dde954ed86886828e5b
cedvdb <cedvandenbosch@gmail.com>
2023-06-27 21:38:55 +0200

    remove unnecessary dep

--- lib/circle_flags.dart
+++ lib/circle_flags.dart
@@ -2,7 +2,6 @@ library circle_flags;
 
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
